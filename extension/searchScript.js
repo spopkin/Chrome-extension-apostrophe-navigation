504ab2bb6c3fbe55b8e857328b2448d59721032f
sjp <sjpopkin@gmail.com>
2017-07-02 19:54:31 -0400

    Add link cycling
    Pressing F2 can be used to cycle forward through the links on the
    page that match the search string.

--- extension/searchScript.js
+++ extension/searchScript.js
@@ -1,12 +1,14 @@
 //alert("extension loaded");
 
-var barOpen = false;
-var searchBarId = "injectedApostropheSearchBar";
-var lastSearch = "";
-var barOpenKey = '\'';
-var barCloseKey = 'Escape';
+//constants
+const searchBarId = "injectedApostropheSearchBar";
+const barOpenKey = '\'';
+const barCloseKey = 'Escape';
 //F3 does this in Firefox, but Chrome intercepts that
-var continueKey = 'F2';
+const continueKey = 'F2';
+
+//are we in the middle of a search now?
+var barOpen = false;
 
 //create the search bar
 var bodyNode = document.body;
@@ -15,6 +17,17 @@ searchBar.nodeType = "text";
 searchBar.id = searchBarId;
 bodyNode.appendChild(searchBar);
 
+//The most recent search query.  It has to be up here,
+//so as to facilitate pickup up where we left off with
+//the F2 key.
+var lastSearch = "";
+
+//The index of the next link to search;
+var searchNext = 0;
+
+//The currently selected link.  It had to be put here,
+//so as to be able to reset its style when selecting a
+//different one.
 var currentLink = null;
 var currentLinkStyle = null;
 
@@ -32,16 +45,22 @@ document.body.addEventListener("keyup", function (keystroke) {
 	else if (keystroke.key == barCloseKey) {
 		//close the search bar
 		hideSearchBar();
+
+		//stub
+		searchNext = 0;
 	}
 	else if (keystroke.key == continueKey) {
 			//continue the search without clearing the last search
 			showSearchBar();
+			searchLinksForString(lastSearch, searchNext);
+			searchNext++;
 	}
 
 	else if (barOpen && document.activeElement.id === searchBarId) {
 		//begin the search feature here
 		lastSearch = getSearchValue();
 		searchLinksForString(lastSearch, 0);
+		searchNext = 1;
 	}
 });
 
@@ -58,7 +77,7 @@ function hideSearchBar() {
 		document.getElementById(searchBarId).style.display = "none";
 		barOpen = false;
 	}
-	resetCurrentLinkStyle();
+	resetCurrentLink();
 }
 
 //Get the current search bar text
@@ -70,8 +89,7 @@ function getSearchValue() {
 // Params: the string to search for, the instance number
 // if you are using the F2 key to iterate
 function searchLinksForString (searchString, instanceNo) {
-	resetCurrentLinkStyle();
-	console.log(searchString);
+	resetCurrentLink();
 	var allLinks = document.getElementsByTagName("a");
 	var filteredLinks = [];
 	for (var i = 0; i < allLinks.length; i++) {
@@ -81,18 +99,24 @@ function searchLinksForString (searchString, instanceNo) {
 		}
 	} 
 	// target instance is filtered[no]
-	currentLink = filteredLinks[instanceNo % filteredLinks.length];
-	currentLinkStyle = currentLink.style;
-	currentLink.style.border = "1px dotted grey";
+	if (filteredLinks.length > 0) {
+		currentLink = filteredLinks[instanceNo % filteredLinks.length];
+		currentLinkStyle = currentLink.style;
+		currentLink.style.border = "1px dotted grey";
+		mostRecent = instanceNo;
+	}
+	else {
+		console.log("no match for: " + lastSearch);
+	}
 	//var selection = window.getSelection();
 	//selection.empty();
 	
-
 	//console.log(currentLink.innerText);
 }
 
-function resetCurrentLinkStyle() {
+function resetCurrentLink() {
 	if (currentLink != null) {
 		currentLink.style = currentLinkStyle;
+		currentLink = null;
 	}
 }
