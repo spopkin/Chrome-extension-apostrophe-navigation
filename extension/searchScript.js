c76a61a711804bfa1a4506492402d7dcd30eb2ae
sjp <sjpopkin@gmail.com>
2017-07-02 13:33:40 -0400

    Make the search bar show/hide
    The feature to show and hide the search bar now works.  The search
    itself still has yet to be implemented.

--- extension/searchScript.js
+++ extension/searchScript.js
@@ -1,8 +1,36 @@
 //alert("extension loaded");
 
-window.addEventListener("load", function() {
-	window.addEventListener("keyup", function (key) {
-//		alert("" + key.getKeyCode);
+var barOpen = false;
 
-	});
+document.body.addEventListener("keyup", function (keystroke) {
+	if (keystroke.key == '\'') {
+		if (keystroke.target.nodeName != "INPUT") {
+			//open the search bar
+
+			var bodyNode = document.body;
+			var searchBar = document.createElement("input");
+			searchBar.nodeType = "text";
+			searchBar.id = "injectedApostropheSearchBar";
+		
+			bodyNode.appendChild(searchBar);
+			barOpen = true;
+		}
+	}
+	else if (keystroke.key == 'Escape') {
+		//close the search bar
+		if (barOpen) {
+			var searchBar = document.getElementById("injectedApostropheSearchBar");
+			document.body.removeChild(searchBar);
+		} 
+	}
+	else if (keystroke.key == 'F2') {
+		//continue the search
+		//F3 does this in Firefox, but Chrome intercepts that
+		alert("F2");	
+	}
+
+	else if {
+		//begin the search feature here
+	}
 });
+
