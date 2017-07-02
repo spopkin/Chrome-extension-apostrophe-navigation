51e632f84455c18f58954789c6e1aed67d381309
sjp <sjpopkin@gmail.com>
2017-07-02 15:52:29 -0400

    Make F2 show the bar
    F2 now shows the search bar, with whatever was last typed into it
    on the current page.  TODO: use session storage to make it remember
    across pages.  TODO: actually search links on the page.

--- extension/searchScript.js
+++ extension/searchScript.js
@@ -1,36 +1,65 @@
 //alert("extension loaded");
 
 var barOpen = false;
+var searchBarId = "injectedApostropheSearchBar";
+var lastSearch = "";
+var barOpenKey = '\'';
+var barCloseKey = 'Escape';
+//F3 does this in Firefox, but Chrome intercepts that
+var continueKey = 'F2';
+
+//function createSearchBar () {
+//open the search bar
+
+var bodyNode = document.body;
+var searchBar = document.createElement("input");
+searchBar.nodeType = "text";
+searchBar.id = searchBarId;
+
+barOpen = true;
+bodyNode.appendChild(searchBar);
+
+searchBar.focus();
+//}
 
 document.body.addEventListener("keyup", function (keystroke) {
-	if (keystroke.key == '\'') {
+	if (keystroke.key == barOpenKey) {
 		if (keystroke.target.nodeName != "INPUT") {
-			//open the search bar
-
-			var bodyNode = document.body;
-			var searchBar = document.createElement("input");
-			searchBar.nodeType = "text";
-			searchBar.id = "injectedApostropheSearchBar";
-		
-			bodyNode.appendChild(searchBar);
-			barOpen = true;
+			lastSearch = "";
+			showSearchBar();
+			document.getElementById(searchBarId).focus();
 		}
 	}
-	else if (keystroke.key == 'Escape') {
+	else if (keystroke.key == barCloseKey) {
 		//close the search bar
 		if (barOpen) {
-			var searchBar = document.getElementById("injectedApostropheSearchBar");
-			document.body.removeChild(searchBar);
+			hideSearchBar();
 		} 
 	}
-	else if (keystroke.key == 'F2') {
-		//continue the search
-		//F3 does this in Firefox, but Chrome intercepts that
-		alert("F2");	
+	else if (keystroke.key == continueKey) {
+		if (!barOpen) {
+			//continue the search without clearing the last search
+			showSearchBar();
+		}
 	}
 
-	else if {
+	else if (barOpen && document.activeElement.id === searchBarId) {
 		//begin the search feature here
+		lastSearch = getSearchValue();
 	}
 });
 
+function showSearchBar() {
+	searchBar.value = lastSearch;
+	document.getElementById(searchBarId).style.display = "block";
+	barOpen = true;
+}
+
+function hideSearchBar() {
+	document.getElementById(searchBarId).style.display = "none";
+	barOpen = false;
+}
+
+function getSearchValue() {
+	return document.getElementById(searchBarId).value;
+}
